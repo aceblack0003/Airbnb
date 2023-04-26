248e6e101a52be0646fd3e7599579af6b12d6754
aceblack0003 <aceblack0003@gmail.com>
2023-04-26 21:35:10 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
+import ClientOnly from './components/ClientOnly'
+import RegisterModal from './components/modals/RegisterModal'
 import Navbar from './components/navbar/Navbar'
 import './globals.css'
 import { Nunito } from 'next/font/google'
+import ToasterProvider from './providers/ToasterProvider'
 
 const font = Nunito({
   subsets: ['latin']
@@ -19,7 +22,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <ToasterProvider/>
+          <RegisterModal/>
+          <Navbar />
+        </ClientOnly>
         {children}
       </body>
     </html>
